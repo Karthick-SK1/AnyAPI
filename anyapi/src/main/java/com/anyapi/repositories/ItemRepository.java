3e9a5a2de18179ac572f7bf6f8c811d4b88da6a7
Karthick_Sha <102850284+Karthick-SK1@users.noreply.github.com>
2024-10-20 15:58:00 +0530

    Delete anyapi directory

--- anyapi/src/main/java/com/anyapi/repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anyapi.repositories;
-
-import com.anyapi.models.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-}
